69dc5f417d524020320a7d6aa9f70d1125af71c5
Deleplace <deleplace2015@gmail.com>
2016-06-29 00:07:00 +0200

    runconcurrent.go with HL

---
+++ 2016/elements-of-web-server-in-go/runconcurrent.go
@@ -0,0 +1,16 @@
+import "sync" // OMIT
+
+// RunConcurrent launches funcs,
+// and waits for their completion.
+func RunConcurrent(funcs ...func()) { // HLdecl
+	var wg sync.WaitGroup // HLwg
+	wg.Add(len(funcs))    // HLwg
+	for _, f := range funcs {
+		f := f // HLtrap
+		go func() {
+			f()
+			wg.Done() // HLwg
+		}()
+	}
+	wg.Wait() // HLwg
+} // HLdecl
